1dd8825ec3e1cb18942f37e847357ce8491f0fef
patchstack <patchstack@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2023-11-20 13:17:14 +0000

    Fixed: issue with the firewall engine processor.
    git-svn-id: https://plugins.svn.wordpress.org/patchstack/trunk@2998926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/patchstack/src/Processor.php
+++ lib/patchstack/src/Processor.php
@@ -278,6 +278,7 @@ class Processor
                                 return true;
                             } else {
                                 $inclusiveHits++;
+                                break 2;
                             }
                         }
                     }
